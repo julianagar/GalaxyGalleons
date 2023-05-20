c3d720a97c88be3e5998ca04583a240887266c30
Julian Garcia <jelogarcia2008@gmail.com>
2023-05-20 12:12:38 -0700

    finished login, created homepage and started work on lessons

--- app.js
+++ app.js
@@ -8,8 +8,25 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+const users = [
+    {name: "Jeff Bezos", password: "Amazon"},
+    {name: "Elon Musk", password: "Tesla"},
+    {name: "Mark Zuckerberg", password: "Facebook"},
+]
+
 app.get("/", function(req, res) {
     res.render("index");
+})
+
+app.post("/", function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+    console.log(username, password)
+    
+    if (username === "Elon Musk" && password === "Tesla") {
+        res.render("homepage", {username: username})
+    } 
+    
 })
 
 app.listen(3000, function() {
