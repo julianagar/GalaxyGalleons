b07111b0c6bc81f56cc0df3396c4e0dcf3ed93ec
Julian Garcia <jelogarcia2008@gmail.com>
2023-05-21 12:11:05 -0700

    added user-progress

--- app.js
+++ app.js
@@ -11,27 +11,45 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let loggedIn = false 
+
 let users = [
-    {name: "Elon Musk", password: "Tesla", progress: [0, 0, 0,  0, 0]},
+    {name: "Simon Ngyuen", password: "ChatGPT", lesson: [
+        0, 0, 0, 0, 0
+    ]},
 ]
 
 app.get("/", function(req, res) {
-    res.render("index");
+    console.log(loggedIn)
+    if (loggedIn === true) {
+        res.render("app", {user: user})
+    } else {
+        res.render("index");
+    }
 })
 
+let user 
+
+
 app.post("/", function(req, res) {
+    
     const username = req.body.username;
     const password = req.body.password;
-    console.log(username, password)
+    // console.log(username, password)
     
     users.forEach(function(login) {
         if (login.name === username && login.password === password) {
-            const theUser = login
-            res.render("app")
+            user = login
+            loggedIn = true
+            console.log(user)
+            
+            res.render("app", {user: user})
         } else {
             res.send("There was a problem logging in. Please try again.")
         }
     })
+    
+    
 })
 
 app.get("/app", function(req, res) {
@@ -39,35 +57,42 @@ app.get("/app", function(req, res) {
 })
 
 const lesson1 = lessonPlans.lessons[0]
-console.log(lesson1)
+// console.log(lesson1)
 
 app.get("/lesson1", function(req, res) {
-    res.render("lesson", {lesson1: lesson1, index: 0})
+    res.render("lesson", {lesson1: lesson1, index: 0, user: user})
 })
 
+app.post("/lesson1", function(req, res) {
+    console.log("hello world")
+    console.log(req.body.submit)
+    res.render("app", {user: user})
+})
+
+
 const lesson2 = lessonPlans.lessons[1]
-console.log(lesson2)
+// console.log(lesson2)
 
 app.get("/lesson2", function(req, res) {
     res.render("lesson", {lesson1: lesson2, index: 1})
 })
 
 const lesson3 = lessonPlans.lessons[2]
-console.log(lesson3)
+// console.log(lesson3)
 
 app.get("/lesson3", function(req, res) {
     res.render("lesson", {lesson1: lesson3, index: 2})
 })
 
 const lesson4 = lessonPlans.lessons[3]
-console.log(lesson4)
+// console.log(lesson4)
 
 app.get("/lesson4", function(req, res) {
     res.render("lesson", {lesson1: lesson4, index: 3})
 })
 
 const lesson5 = lessonPlans.lessons[4]
-console.log(lesson5)
+// console.log(lesson5)
 
 app.get("/lesson5", function(req, res) {
     res.render("lesson", {lesson1: lesson5, index: 4})
