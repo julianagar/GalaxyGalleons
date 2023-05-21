efa51120c6174c060ec3267a2f089e9512af4c77
Julian Garcia <jelogarcia2008@gmail.com>
2023-05-20 22:20:36 -0700

    dictionary + planet explorer added. good night 😴

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const lessonPlans = require('./lessonplans.json')
+const planets = require('./planets.json')
+const dictionary = require('./dictionary.json')
 const express = require("express");
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
@@ -9,10 +11,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const users = [
-    {name: "Jeff Bezos", password: "Amazon"},
-    {name: "Elon Musk", password: "Tesla"},
-    {name: "Mark Zuckerberg", password: "Facebook"},
+let users = [
+    {name: "Elon Musk", password: "Tesla", progress: [0, 0, 0,  0, 0]},
 ]
 
 app.get("/", function(req, res) {
@@ -24,10 +24,14 @@ app.post("/", function(req, res) {
     const password = req.body.password;
     console.log(username, password)
     
-    if (username === "." && password === ".") {
-        res.redirect("/app")
-    } 
-    
+    users.forEach(function(login) {
+        if (login.name === username && login.password === password) {
+            const theUser = login
+            res.render("app")
+        } else {
+            res.send("There was a problem logging in. Please try again.")
+        }
+    })
 })
 
 app.get("/app", function(req, res) {
@@ -40,6 +44,44 @@ console.log(lesson1)
 app.get("/lesson1", function(req, res) {
     res.render("lesson", {lesson1: lesson1, index: 0})
 })
+
+const lesson2 = lessonPlans.lessons[1]
+console.log(lesson2)
+
+app.get("/lesson2", function(req, res) {
+    res.render("lesson", {lesson1: lesson2, index: 1})
+})
+
+const lesson3 = lessonPlans.lessons[2]
+console.log(lesson3)
+
+app.get("/lesson3", function(req, res) {
+    res.render("lesson", {lesson1: lesson3, index: 2})
+})
+
+const lesson4 = lessonPlans.lessons[3]
+console.log(lesson4)
+
+app.get("/lesson4", function(req, res) {
+    res.render("lesson", {lesson1: lesson4, index: 3})
+})
+
+const lesson5 = lessonPlans.lessons[4]
+console.log(lesson5)
+
+app.get("/lesson5", function(req, res) {
+    res.render("lesson", {lesson1: lesson5, index: 4})
+})
+
+app.get("/dictionary", function(req, res) {
+    res.render("dictionary", {dictionary: dictionary})
+})
+
+const Mercury = planets.planets[0]
+
+app.get("/Mercury", function(req, res) {
+    res.render("planets", {planet: Mercury});
+});
 
 app.listen(3000, function() {
     console.log("Server started successfully");
